e945244718c254259d4895b4b2db5495da722ed7
amtkxa <scarlet0211@outlook.jp>
2018-06-19 18:14:42 +0900

    カレンダーのヘッダ文字列のサイズを変更

--- app/javascript/packs/components/about.vue
+++ app/javascript/packs/components/about.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
     <p>Vue.js と Ruby on Rails. のお勉強をしています。</p>
-    <p><a href="https://github.com/naoki85/todo_app_with_vue_and_rails" target="_blank">こちら</a>を参考にさせて頂いています。</p>
   </div>
 </template>
