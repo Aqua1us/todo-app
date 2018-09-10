0c4e1761c16f7b36d72b641eb2fead2ac5cee0fa
AKAMATSU TAKUYA <scarlet0211@outlook.jp>
2018-09-10 10:18:06 +0900

    編集画面に保存機能を追加

--- app/javascript/packs/components/edit.vue
+++ app/javascript/packs/components/edit.vue
@@ -4,20 +4,25 @@
     <form class="col s12">
       <div class="row">
         <div class="input-field col s12">
-          <input placeholder="タスク名" type="text" id="name" ref="name">
+          <input v-model="name" placeholder="タスク名" type="text" id="name" ref="name">
           <label for="name" class="active">タスク名</label>
         </div>
         <div class="input-field col s6">
-          <datepicker ref="startdate" id="startdate" :format="this.customFormatter" placeholder="開始日"></datepicker>
+          <datepicker v-model="startdate" ref="startdate" id="startdate" :format="this.customFormatter" placeholder="開始日"></datepicker>
           <label for="startdate" class="active">開始日</label>
         </div>
         <div class="input-field col s6">
-          <datepicker ref="enddate" id="enddate" :format="this.customFormatter" placeholder="終了日"></datepicker>
+          <datepicker v-model="enddate" ref="enddate" id="enddate" :format="this.customFormatter" placeholder="終了日"></datepicker>
           <label for="enddate" class="active"> 終了日</label>
         </div>
         <div class="input-field col s12">
           <input placeholder="メモ" type="text" id="memo" ref="memo">
           <label for="memo" class="active">メモ</label>
+        </div>
+        <div class="input-field col s12">
+          <button class="btn waves-effect waves-light " v-on:click="updateTask">
+            保存する<i class="material-icons right">save</i>
+          </button>
         </div>
       </div>
     </form>
@@ -34,11 +39,12 @@
       'datepicker': Datepicker
     },
     mounted: function () {
-      this.showTask(this.$router.currentRoute.params.task_id)
+      this.showTask()
     },
     methods: {
       // タスクの表示
-      showTask: function (task_id) {
+      showTask: function () {
+        let task_id = this.$router.currentRoute.params.task_id
         axios.get('/api/tasks/' + task_id).then((response) => {
           this.$refs.name.value = response.data.task.name
           this.$refs.startdate.value = response.data.task.startdate
@@ -46,7 +52,16 @@
         }, (error) => {
           console.log(error)
         })
-      }
+      },
+      // タスクの保存
+      updateTask: function () {
+        let task_id = this.$router.currentRoute.params.task_id
+        // var done = el.checked ? 1 : 0;
+        axios.put('/api/tasks/' + task_id, { task: { name: this.name, startdate: this.startdate, enddate: this.enddate } }).then((response) => {
+        }, (error) => {
+          console.log(error);
+        });
+      },
     }
   }
 </script>
